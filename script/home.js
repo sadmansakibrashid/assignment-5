b23d664bc680bac9d214c7a6e0e37e032b60cd60
sadmansakibrashid <sadmansakibrashid@gmail.com>
2026-03-14 20:56:59 +0600

    adding green border top to every card

--- script/home.js
+++ script/home.js
@@ -15,7 +15,7 @@ const displayLesson = (lessons)=>{
     const btnDiv=document.createElement("div");
     btnDiv.innerHTML=`
     <div onclick="loadWordDetail(${lesson.id})" class="bg-white">
-  <div class=" py-5 px-5 flex justify-between">
+  <div class="border-t-4 border-green-500 py-5 px-5 flex justify-between">
         <div>
             <img src="./assets/Open-Status.png" alt="">
         </div>
@@ -41,11 +41,51 @@ const displayLesson = (lessons)=>{
 loadLessons();
 
 const loadWordDetail=async(id)=>{
-    const url =`https://phi-lab-server.vercel.app/api/v1/lab/issue/{id}${id}`;
+    const url =`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
     console.log(url);
     const res= await fetch(url);
     const details=await res.json();
-    console.log(details);
+    displayWordDetail(details);
+};
+
+const displayWordDetail = (word) => {
+
+  const detailBox = document.getElementById("details-container");
+
+  detailBox.innerHTML = `
+    <div>
+        <h2 class="font-semibold text-xl">${word.data.title}</h2>
+    </div>
+
+    <div class="flex gap-3 items-center my-3">
+        <button class="btn btn-success btn-sm rounded-full">${word.status}</button>
+        <p>Opened by ${word.data.author}</p>
+        <p>${new Date(word.data.createdAt).toLocaleString()}</p>
+    </div>
+
+    <div class="flex gap-2 my-2">
+       
+  <button class="btn btn-soft btn-error">${word.data.labels[0]}</button>
+  <button class="btn btn-soft btn-success">${word.data.labels[1]}</button>
+</div>
+    
+
+    <p class="my-3">${word.data.description}</p>
+
+    <div class="flex justify-between bg-gray-100 p-4 rounded-lg">
+        <div>
+            <p class="text-gray-500">Assignee</p>
+            <h3 class="font-semibold">${word.data.author}</h3>
+        </div>
+
+        <div>
+            <p class="text-gray-500">Priority</p>
+            <button class="btn btn-error btn-xs">${word.data.priority}</button>
+        </div>
+    </div>
+  `;
+
+  document.getElementById("my_modal_5").showModal();
 };
 
 
